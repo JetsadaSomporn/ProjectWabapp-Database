3f6c0ad0e1039aeeff422ba56c7401c54f33f8aa
nnitthawan <nitthawan.p@kkumail.com>
2023-09-15 18:17:07 +0700

    css

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,9 +1,13 @@
+<link href="http://127.0.0.1:8000/css/profile.css" rel="stylesheet">
 <x-app-layout>
-@push('styles')
-    <link rel="stylesheet" href="">
-@endpush
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+<div class="body-navigation">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+        <div class="back">
+            
+            <div class="content">
+                กล่องนี้
+            </div>
+
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-profile-information-form')
@@ -21,6 +25,9 @@
                     @include('profile.partials.delete-user-form')
                 </div>
             </div>
+            </div>
         </div>
+    </div>
+        
     </div>
 </x-app-layout>
