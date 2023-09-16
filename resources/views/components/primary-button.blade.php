f660effe91d9a792907fc0218fe1a31a102e5aea
nnitthawan <nitthawan.p@kkumail.com>
2023-09-16 23:10:17 +0700

    css

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,3 +1,14 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150']) }}>
+<style>
+    #bt{
+        background-color: white;
+    }
+    #bt:hover{
+        background-color: #7D9BFF;
+        border:1px solid white;
+        border-radius: 5px;
+        color:white;
+    }
+</style>
+<button id="bt" {{ $attributes->merge(['type' => 'submit','class' => 'inline-flex items-end px-6 py-2 border border-transparent rounded-md font-semibold text-xs text-black  uppercase tracking-widest  focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-red-800 transition ease-in-out duration-150']) }}>
     {{ $slot }}
 </button>
--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,33 +1,34 @@
 <link href="http://127.0.0.1:8000/css/profile.css" rel="stylesheet">
 <x-app-layout>
 <div class="body-navigation">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-        <div class="back">
-            
+        <div class="background">
             <div class="content">
-                กล่องนี้
-            </div>
-
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
+                <div class="bg">
+                <img class="rounded-full  pb-0"  src="http://127.0.0.1:8000/../images/Edit.png" alt="image edit" id="iconEdit">
+                <svg class="gray-circle" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30" fill="none">
+                <circle cx="15" cy="15" r="15" fill="#F2F2F2"/>
+                </svg>
+                    <img class="rounded-full  pb-0"  src="http://127.0.0.1:8000/../images/ProfileIcon.jpg" alt="image profile" id="ProfileIcon">
+                    <svg class="white-circle" xmlns="http://www.w3.org/2000/svg" width="123" height="123" viewBox="0 0 123 123" fill="none">
+                    <circle cx="61.5" cy="61.5" r="61.5" fill="white"/>
+                    </svg>
+                </div>
+                <div class="content-item">
                     @include('profile.partials.update-profile-information-form')
                 </div>
-            </div>
-
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
+    
+                <div class="content-item">
                     @include('profile.partials.update-password-form')
                 </div>
-            </div>
-
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
+    
+                <div class="content-item">
+                    @include('profile.partials.update-password-form')
+                </div>
+    
+                <div class="content-item">
+                    @include('profile.partials.delete-user-form') 
                 </div>
             </div>
-            </div>
-        </div>
-    </div>
-        
-    </div>
+        </div>   
+</div>        
 </x-app-layout>
